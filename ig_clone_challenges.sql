41aa7ba17ba11e45e6b92c3a5d0ddb6ce8bfedc2
Noah D <sheltielover432@gmail.com>
2020-08-29 22:09:01 +0000

    Challenge 2

--- ig_clone_challenges.sql
+++ ig_clone_challenges.sql
@@ -1,8 +1,20 @@
 USE ig_clone; 
 
+###############################
 # Challenge 1
 # Find the 5 oldest users
+###############################
 
-SELECT * FROM users 
-ORDER BY created_at 
-LIMIT 5;
+-- SELECT * FROM users 
+-- ORDER BY created_at 
+-- LIMIT 5;
+
+###############################
+# Challenge 2
+# What day of the week do most people register?
+###############################
+
+SELECT DAYNAME(created_at) as day, COUNT(*) as total FROM users
+GROUP BY day
+ORDER BY total DESC
+LIMIT 2;
